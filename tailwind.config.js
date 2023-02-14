3d352ceae2387c598d4986a495dcc05705ad2264
BRYAN LEONG <bryan.leong.2020@scis.smu.edu.sg>
2023-02-14 18:41:04 +0800

    Update: MongoDB initialised

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  // purge: [".src/**/*.{js,hsx,ts,tsx}", "./public/index.html"],
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  // purge: ["./src/**/*.{js,hsx,ts,tsx}", "./public/index.html"],
+  content: [
+    "./src/*.{js,hsx,ts,tsx,html}",
+    "./src/**/*.{html,js}",
+    "./public/index.html",
+    "./src/**/*.{js,hsx,ts,tsx}",
+    "./src/App.tsx",
+  ],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
